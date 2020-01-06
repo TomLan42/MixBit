3d04a6ec9f46636ba5c68d1afd7d0b7e2c55a20d
tomlan <lanhaochong1@sensetime.com>
2020-01-06 21:57:08 +0800

    shell

--- run_temp.sh
+++ run_temp.sh
@@ -1,14 +1,49 @@
-python cifar_train_eval.py --model_name vgg \
-        --wbit_code 1 8 1 1 4 1 \
-        --abit_code 1 8 1 1 4 1 \
-        --ratio_code 1 1 1 1 1 1
+# python cifar_train_eval.py --model_name vgg \
+#         --wbit_code 1 8 1 1 4 1 \
+#         --abit_code 1 8 1 1 4 1 \
+#         --ratio_code 1 1 1 1 1 1
 
-python cifar_train_eval.py --model_name vgg \
-        --wbit_code 1 4 4 1 4 4 \
-        --abit_code 1 4 4 1 4 4 \
-        --ratio_code 1 1 1 1 1 1
+# python cifar_train_eval.py --model_name vgg \
+#         --wbit_code 1 4 4 1 4 4 \
+#         --abit_code 1 4 4 1 4 4 \
+#         --ratio_code 1 1 1 1 1 1
 
-python cifar_train_eval.py --model_name vgg \
-        --wbit_code 1 4 4 1 4 4 \
-        --abit_code 1 4 4 1 4 4 \
-        --ratio_code 1 1 1 1 1 1
+# python cifar_train_eval.py --model_name vgg \
+#         --wbit_code 1 4 4 1 4 4 \
+#         --abit_code 1 4 4 1 4 4 \
+#         --ratio_code 1 1 1 1 1 1
+
+
+python cifar_train_eval.py --model_name vgg  --max_epochs 30 --lr 0.01 \
+        --wbit_code 1 1	1 1 4 1 \
+        --abit_code 1 1	1 1 4 1 \
+        --ratio_code 1 1 1 1 1 1 \
+        --pretrain --pretrain_dir ./ckpt/vgg[111111][111111][1.01.01.01.01.01.0]cifar100/checkpoint.t7
+
+
+python cifar_train_eval.py --model_name vgg  --max_epochs 30 --lr 0.01 \
+        --wbit_code 1 1	1 1 1 4 \
+        --abit_code 1 1	1 1 1 4 \
+        --ratio_code 1 1 1 1 1 1 \
+        --pretrain --pretrain_dir ./ckpt/vgg[111111][111111][1.01.01.01.01.01.0]cifar100/checkpoint.t7
+
+
+python cifar_train_eval.py --model_name vgg  --max_epochs 30 --lr 0.01 \
+        --wbit_code 1 1	1 4 1 1 \
+        --abit_code 1 1	1 4 1 1 \
+        --ratio_code 1 1 1 1 1 1 \
+        --pretrain --pretrain_dir ./ckpt/vgg[111111][111111][1.01.01.01.01.01.0]cifar100/checkpoint.t7
+
+
+python cifar_train_eval.py --model_name vgg  --max_epochs 30 --lr 0.01 \
+        --wbit_code 1 1	4 1 1 1 \
+        --abit_code 1 1	4 1 1 1 \
+        --ratio_code 1 1 1 1 1 1 \
+        --pretrain --pretrain_dir ./ckpt/vgg[111111][111111][1.01.01.01.01.01.0]cifar100/checkpoint.t7
+
+
+python cifar_train_eval.py --model_name vgg  --max_epochs 30 --lr 0.01 \
+        --wbit_code 1 4	1 1 1 1 \
+        --abit_code 1 4	1 1 1 1 \
+        --ratio_code 1 1 1 1 1 1 \
+        --pretrain --pretrain_dir ./ckpt/vgg[111111][111111][1.01.01.01.01.01.0]cifar100/checkpoint.t7
